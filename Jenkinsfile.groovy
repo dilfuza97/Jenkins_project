70dd3a29ebb5b06fcad50b814c5434a8f3f3db7f
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-28 19:11:16 -0500

    create

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,6 +6,9 @@ node{
     stage("Install git"){
         sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip -y"
     }
+    stage("Remove repo"){
+        sh "ssh  ec2-user@${ENVIR} sudo  rm -rf /home/ec2-user/flask-examples"
+    }
     stage("Pull Repo"){
         sh "ssh  ec2-user@${ENVIR} git clone https://github.com/miguelgrinberg/flask-examples.git"
     }
@@ -15,7 +18,7 @@ node{
         sh "echo Hello"
     }
     stage("Pip Install"){
-        sh "ssh  ec2-user@${ENVIR} pip install -r /home/ec2-user/flask-examples/requirements.txt"
+        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/flask-examples/requirements.txt"
     }
     stage("Run App"){
         sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/flask-examples/01-hello-world/hello.py"
