e19904635d885bb26936e4e1c2d6299cde3ee544
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-28 20:25:33 -0500

    create

--- Stompath/Jenkinsfile.groovy
+++ Stompath/Jenkinsfile.groovy
@@ -7,20 +7,21 @@ node{
         sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip -y"
     }
     stage("Remove repo"){
-        sh "ssh  ec2-user@${ENVIR} sudo  rm -rf /home/ec2-user/Flaskex"
+        sh "ssh  ec2-user@${ENVIR} sudo  rm -rf /home/ec2-user/stormpath-flask-sample"
+    }
+    stage("Remove PID"){
+        sh "ssh  ec2-user@${ENVIR} sudo kill $(sudo lsof -i:5000   | awk '{print $2}' | grep [[:digit:]]) "
     }
     stage("Pull Repo"){
-        sh "ssh  ec2-user@${ENVIR} git clone https://github.com/dilfuza97/stormpath-flask-sample.git"
+        sh "ssh  ec2-user@${ENVIR} git clone https://github.com/dilfuza97/stormpath-flask-sample.git 2> /dev/null"
     }
     stage("Install Requirements"){
-        //sh "virtualenv /tmp/venv"
-        //sh ". /tmp/venv/bin/activate"
         sh "echo Hello"
     }
     stage("Pip Install"){
-        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/Flaskex/requirements.txt"
+        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/stormpath-flask-sample/requirements.txt"
     }
     stage("Run App"){
-        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/Flaskex/app.py"
+        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/stormpath-flask-sample/app.py"
     }
 }
