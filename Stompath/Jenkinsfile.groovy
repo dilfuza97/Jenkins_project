af3ae490c3e605de2d1a569cebbcc1c0f9e2ed33
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-28 20:17:05 -0500

    create

--- Stompath/Jenkinsfile.groovy
+++ Stompath/Jenkinsfile.groovy
@@ -7,22 +7,20 @@ node{
         sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip -y"
     }
     stage("Remove repo"){
-        def exists = fileExists 'stormpath-flask-sample'
-        if (exists) {
-            echo "exists"
-        } else {
-        }
+        sh "ssh  ec2-user@${ENVIR} sudo  rm -rf /home/ec2-user/Flaskex"
     }
     stage("Pull Repo"){
         sh "ssh  ec2-user@${ENVIR} git clone https://github.com/dilfuza97/stormpath-flask-sample.git"
     }
     stage("Install Requirements"){
+        //sh "virtualenv /tmp/venv"
+        //sh ". /tmp/venv/bin/activate"
         sh "echo Hello"
     }
     stage("Pip Install"){
-        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/stormpath-flask-sample/requirements.txt"
+        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/Flaskex/requirements.txt"
     }
     stage("Run App"){
-        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/stormpath-flask-sample/app.py"
+        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/Flaskex/app.py"
     }
 }
